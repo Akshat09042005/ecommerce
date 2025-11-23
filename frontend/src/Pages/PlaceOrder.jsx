1c299b9911b42d53e40227f3a94cfa22ced940ce
Akshat Agrawal <agrawalakshat2005@gmail.com>
2025-11-23 19:53:20 +0530

    bugs

--- frontend/src/Pages/PlaceOrder.jsx
+++ frontend/src/Pages/PlaceOrder.jsx
@@ -224,7 +224,7 @@ const PlaceOrder = () => {
           <button
             type='submit'
             className='bg-black text-white px-16 py-3 text-sm'
-          // onClick={()=>navigate('/orders')}
+            onClick={()=>navigate('/orders')}
           >
             PLACE ORDER
           </button>
